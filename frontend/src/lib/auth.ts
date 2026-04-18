db33ff8830db9fe0846ecb389ea8820911b80011
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2026-04-18 20:21:02 +0100

    added better auth dashboard connection

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,5 +1,7 @@
+import { sentinelClient } from "@better-auth/infra/client";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_API_URL ?? "https://api.riotinto.henriquesf.me",
+  plugins: [sentinelClient()],
 });
